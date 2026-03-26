d363050a1a2ff7f3656f368cc808432661378168
Hitraj1601 <hitrajsinhchavda1618@gmail.com>
2026-03-26 20:33:30 +0530

    chore: sync backend updates

--- src/app.js
+++ src/app.js
@@ -14,7 +14,8 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:5173',
+    // origin: 'http://localhost:5173',
+    origin: 'https://instazometo-frontend.vercel.app/',
     credentials: true
 }))
 
